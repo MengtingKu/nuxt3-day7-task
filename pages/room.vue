48bd4f68e44d3c123f68fcec3c45fc90c41f8495
kuku <polorbear520@gmail.com>
2024-11-17 19:43:11 +0800

    Day 7 - 嵌套式路由、 useRouter & useRoute

---
+++ pages/room.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+    <NuxtPage />
+</template>
+
+<style scoped>
+.card-img-top {
+    object-fit: cover;
+    height: 200px;
+    max-width: 100%;
+}
+</style>
---
+++ pages/room/_id.vue
@@ -0,0 +1,16 @@
+<script setup>
+const router = useRouter();
+</script>
+
+<template>
+    <h2>房型詳細頁面</h2>
+    <button type="button" class="btn" @click="router.go(-1)">前往上一頁</button>
+</template>
+
+<style lang="scss" scoped>
+.btn {
+    padding: 10px;
+    margin: 10px 0;
+    background: #fff;
+}
+</style>
